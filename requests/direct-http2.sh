89aa89a491507251fea49fd9adf0ec425ec7ceef
anthony <antonsdl@gmail.com>
2018-03-15 21:27:54 +0300

    moved requests into separate folder

---
+++ requests/direct-http2.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+url=https://localhost:8080/upload
+
+# -w "@curl-format.txt" - format output
+# --insecure - trust unverified SSL sertificate
+# -F - multipart data part
+# -v - verbose output
+
+echo "Direct requests to application at localhost:8080"
+echo "--- HTTP 2 ---"
+echo "Nginx proxy. GET"
+curl --http2-prior-knowledge -w "@curl-format.txt" ${url} --insecure -v
+
+
+printf "\n\n\nCreating 50mb file named duck\n"
+mkfile -n 50m duck
+
+echo "Nginx proxy. POST. Multipart data 50mb"
+curl --http2-prior-knowledge -w "@curl-format.txt" -F "name=duck" -F "file=@duck" ${url} --insecure -v
